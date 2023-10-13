2a5b00afa497b7be11202346820d7842ef9ab8bd
Vijay6868 <saroyav3@gmail.com>
2023-10-14 11:41:13 +1300

    Trees folder added

---
+++ Trees/Node1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trees
+{
+    class Node1
+    {
+        public int value;
+        public Node1 left;
+        public Node1 right;
+
+        public void show()
+        {
+            Console.Write("[");
+            Console.Write(value);
+            Console.Write("]"); 
+        }
+    }
+}
---
+++ Trees/Tree.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trees
+{
+    class Tree
+    {
+        public Node1 root;
+
+        public Tree()
+        {
+            root =null;
+        }
+        public Node1 ReturnRoot()
+        {
+            return root;
+        }
+        public void Insert(int x)
+        {
+            Node1 newNode = new Node1();
+            newNode.value = x;
+
+            if(root == null)
+            {
+                root = newNode;
+            }
+            else
+            {
+                Node1 current = root;
+                Node1 parent ;
+
+                while (true)
+                {
+                    parent = current;
+                }
+            }
+        }
+    }
+}
