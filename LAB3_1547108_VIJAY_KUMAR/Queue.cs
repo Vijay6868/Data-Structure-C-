bd843c4b86577681568b8f95d702fee75aea6035
Vijay6868 <saroyav3@gmail.com>
2023-10-19 08:01:45 +1300

    Lab 3 added

---
+++ LAB3_1547108_VIJAY_KUMAR/Queue.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB3_1547108_VIJAY_KUMAR
+{
+    class Queue<T>
+    {
+        private class Node
+        {
+            public T Data { get; }
+            public Node Next { get; set; }
+
+            public Node(T data)
+            {
+                Data = data;
+                Next = null;
+            }
+        }
+
+        private Node front; // Reference to the front node of the queue
+        private Node rear; // Reference to the rear node of the queue
+
+        public int Count { get; private set; } // Number of items in the queue
+
+        public Queue()
+        {
+            front = null;
+            rear = null;
+            Count = 0;
+        }
+
+        public void Enqueue(T item)
+        {
+            Node newNode = new Node(item);
+
+            if (rear == null)
+            {
+                // If the queue is empty, set both front and rear to the new node
+                front = newNode;
+                rear = newNode;
+            }
+            else
+            {
+                // Otherwise, add the new node to the rear of the queue
+                rear.Next = newNode;
+                rear = newNode;
+            }
+
+            Count++;
+        }
+
+        public T Dequeue()
+        {
+            if (front == null)
+            {
+                throw new InvalidOperationException("Queue is empty.");
+            }
+
+            T dequeuedItem = front.Data;
+            front = front.Next;
+
+            if (front == null)
+            {
+                // If front becomes null after dequeuing, the queue is now empty
+                rear = null;
+            }
+
+            Count--;
+            return dequeuedItem;
+        }
+
+        public T Peek()
+        {
+            if (front == null)
+            {
+                throw new InvalidOperationException("Queue is empty.");
+            }
+
+            // Return the data of the front node without dequeuing it
+            return front.Data;
+        }
+
+        public bool IsEmpty()
+        {
+            return Count == 0;
+        }
+    }
+}
---
+++ LAB3_1547108_VIJAY_KUMAR/Test.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace LAB3_1547108_VIJAY_KUMAR
+{
+    class Test
+    {
+        /// <summary>
+        /// driver code
+        /// </summary>
+      
+        static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>();
+
+            while (true)
+            {
+                Console.WriteLine("Enter 'e' to enqueue, 'd' to dequeue, 'p' to peek, 'q' to quit:");
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "e":
+                        Console.WriteLine("Enter the item to enqueue:");
+                        if (int.TryParse(Console.ReadLine(), out int item))
+                        {
+                            queue.Enqueue(item);
+                            Console.WriteLine("Item enqueued.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid input. Please enter an integer.");
+                        }
+                        break;
+                    case "d":
+                        try
+                        {
+                            int dequeuedItem = queue.Dequeue();
+                            Console.WriteLine("Dequeued item: " + dequeuedItem);
+                        }
+                        catch (InvalidOperationException e)
+                        {
+                            Console.WriteLine("Error: " + e.Message);
+                        }
+                        break;
+                    case "p":
+                        try
+                        {
+                            int frontItem = queue.Peek();
+                            Console.WriteLine("Front item: " + frontItem);
+                        }
+                        catch (InvalidOperationException e)
+                        {
+                            Console.WriteLine("Error: " + e.Message);
+                        }
+                        break;
+                    case "q":
+                        Console.WriteLine("Exiting...");
+                        return;
+                    default:
+                        Console.WriteLine("Invalid choice.");
+                        break;
+                }
+
+
+
+                Console.WriteLine("Queue count: " + queue.Count);
+                Console.WriteLine();
+            }
+        }
+    }
+}
