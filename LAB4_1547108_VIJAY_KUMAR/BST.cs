6ba8bb3952e79f0b6bbdd12a88863e6f9cba5817
Vijay6868 <saroyav3@gmail.com>
2023-10-23 12:36:10 +1300

    lab 4 added

---
+++ LAB4_1547108_VIJAY_KUMAR/BST.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB4_1547108_VIJAY_KUMAR
+{
+    public class BST
+    {
+        private Node root;
+        private class Node // inner node class to show in the tree
+        {
+            public int value;
+            public Node left;
+            public Node right;
+
+            public Node(int value)
+            {
+                this.value = value;
+                left = null;
+                right = null;
+            }
+            
+        }
+        public BST()
+        {
+            root = null;
+        }
+
+        public void Insert(int value)// inserting new node with given value in tree
+        {
+            Node newNode = new Node(value);
+
+            if(root == null)// while tree is empty, new node will be root node
+            {
+                root = newNode;
+            }
+            else
+            {
+                Node current = root;
+                Node parent;
+
+                while (true)// identifiying the correct position for the new node through transverse
+                {
+                    parent = current;
+
+                    if(value < current.value)
+                    {
+                        current = current.left;
+
+                        if(current == null)// if reached to leaf node, insert the new node here
+                        {
+                            parent.left = newNode;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        current = current.right;
+
+                        if (current == null)
+                        {
+                            parent.right = newNode;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// transverse through left and right node unitl reach to leaf node
+        /// initial node is root node
+        /// </summary> 
+        /// <returns>present value else false</returns>
+        public bool Find(int value)
+        {
+            Node current = root;
+
+            while(current != null) // unitl it doesn't reach to leaf node
+            {
+                if(value == current.value)
+                {
+                    return true;
+                }
+                else if(value < current.value) 
+                {
+                    current = current.left;
+                }
+                else
+                {
+                    current = current.right;
+                }
+            }
+            return false;
+            
+
+        }
+        /// <summary>
+        /// inorder traverse to sort values in binary search tree
+        /// Initial node value is root
+        /// </summary>
+        
+        private void InorderTraversal(Node node)
+        {
+            if (node != null)
+            {
+                
+                InorderTraversal(node.left);
+                Console.Write(node.value + " ");
+
+                
+                InorderTraversal(node.right);
+            }
+        }
+        public void Sort()
+        {
+            InorderTraversal(root);
+        }
+       /// <summary>
+       /// tranverse through right nodes until reaches leaf node
+       /// </summary>
+       /// <returns>maximum value in binary search tree </returns>
+        public int Max()
+        {
+            if (root == null)
+            {
+                throw new Exception("Tree is empty");
+            }
+
+            Node current = root;
+
+            while (current.right != null)
+            {
+                current = current.right;
+            }
+
+            return current.value;
+        }
+        /// <summary>
+        /// tranverse through left nodes until reaches leaf node
+        /// </summary>
+        /// <returns>minimum value in binary search tree </returns>
+        public int Min()
+        {
+            if (root == null)
+            {
+                throw new Exception("Tree is empty");
+            }
+
+            Node current = root;
+
+            while (current.left != null)
+            {
+                current = current.left;
+            }
+
+            return current.value;
+        }
+       
+    } 
+
+   
+}
---
+++ LAB4_1547108_VIJAY_KUMAR/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+
+namespace LAB4_1547108_VIJAY_KUMAR
+{// Vijay Kumar 1547108 - LAB4
+    class Program
+    {
+        /// <summary>
+        /// dirver code - add integers randomly
+        /// search given number (user input) in binary search tree
+        /// sorts tree in ascending order
+        /// finds smallest and largest value in tree
+        /// testing  function
+        /// generate random numbers and inserts into tree
+        /// </summary>
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(">>>>Binary Search Tree Demonstration<<<<");
+
+            BST bst = new BST();
+            Console.WriteLine("Integers inserted randomly: ");
+            for (int i = 0; i <= 10; i++)
+            {
+                int num = genRandNum();
+                duplicate(bst, num);
+                
+            }
+
+            Console.WriteLine("max number in Tree:");
+            Console.WriteLine(bst.Max());
+
+            Console.WriteLine("min number in Tree:");
+            Console.WriteLine(bst.Min());
+
+           
+            bst.Sort();
+            Console.WriteLine();
+
+            userInput(bst);
+
+        }
+        /// <summary>
+        /// genrates random number b/w 10 and 99
+        /// </summary>
+        /// <returns>random number b/w 10 and 99</returns>
+        public static int genRandNum()
+        {
+            Random rd = new Random();
+            int num = rd.Next(10, 99);
+
+            
+
+            return num;
+        }
+        /// <summary>
+        /// allows users input
+        /// </summary>
+        
+        public static void userInput(BST bst)
+        {
+
+            while (true)
+            {
+                Console.WriteLine("\nEnter [i] to insert, [f] to find, [l] to find largest number, " +
+                "[s] to find smallest number, [sort] to sort the tree, [q] to quit");
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "i":
+                        Console.WriteLine("enter integer value to insert in tree");
+                        if (int.TryParse(Console.ReadLine(), out int item))
+                        {
+                            duplicate(bst, item);
+                        }
+                        else
+                        {
+                            Console.WriteLine("invalid input, please enter a integer");
+                        }
+                        break;
+                    case "f":
+                        Console.WriteLine("enter a integer to find it in tree");
+                        if(int.TryParse(Console.ReadLine(), out int item1))
+                        {
+                            bool result = bst.Find(item1);
+                            printFind(result);
+                        }
+                        else
+                        {
+                            Console.WriteLine("invalid input, please enter a integer");
+                        }
+                        break;
+                    case"l":
+                        Console.WriteLine(bst.Max());
+                        break;
+                    case "s":
+                        Console.WriteLine(bst.Min());
+                        break;
+                    case "sort":
+                        bst.Sort();
+                        break;
+                    case "p":
+                        Console.WriteLine("Printing in ascending order:");
+                        bst.Sort();
+                        break;
+                    case"q":
+                            return;
+                    default:
+                        Console.WriteLine("invalid choice");
+                        break;
+
+                            
+                }
+
+            }
+        }/// <summary>
+        /// display apporpraite message if value found in tree or not
+        /// </summary>
+        /// <param name="result"></param>
+        public static void printFind(bool result) {
+
+            if (result)
+            {
+                Console.WriteLine("\nValue found in Tree\n");
+            }
+            else
+            {
+                Console.WriteLine("\nvalue not found\n");
+            }
+        }/// <summary>
+        /// check duplicate value in tree
+        /// </summary>
+  
+        public static void duplicate(BST bst ,int num)
+        {
+            bool numExist = bst.Find(num);// check if number already present? to avoid duplication
+            if (numExist)// if present display message
+            {
+                Console.WriteLine("Number already present");
+            }
+            else// else insert the number
+            {
+
+                bst.Insert(num);
+                Console.WriteLine(num);
+            }
+            
+        }
+    }
+}
